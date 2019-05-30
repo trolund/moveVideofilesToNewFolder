1382deecaff1b963e55d02be99ab25ac782dbe1e
trolund@gmail.com <vxh77kdp>
2019-05-30 22:33:46 +0200

    args input

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import os
 import shutil
+import sys
 
-oldpath = 'path with video'
-newpath = r'new folder to move det video'
+print("Searching for movies at:")
+oldpath = sys.argv[1]
+print(oldpath)
+print("Moving files to:")
+newpath = sys.argv[2]
+print(newpath)
 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
+    print(newpath + "dir created")
 
+print("Files being moved:\n")
 for root, dirs, files in os.walk(oldpath):
         for file in files:
             if file.endswith(".avi") or file.endswith(".mkv") or file.endswith(".mp4"): # add or file.endswith(".mp4") for more fileformats
